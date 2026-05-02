285afa696f8c6306e87845b45d22b7fb077c844c
csaguile <114958075+csaguile@users.noreply.github.com>
2026-05-01 23:57:37 -0700

    Delete asgn2/webgl-utils.js

--- asgn2/webgl-utils.js
+++
@@ -1,6 +0,0 @@
-// Minimal WebGLUtils helper for CSE160-style projects.
-var WebGLUtils = {
-  setupWebGL: function(canvas) {
-    return canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-  }
-};
